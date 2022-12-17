967e762bc3bf227e507228441b50407266ae0234
Amril Azim <amrilazim@Amrils-MacBook-Pro.local>
2022-12-17 14:10:45 +0800

    COMMIT

--- resources/js/Components/Frontend/Skills.vue
+++ resources/js/Components/Frontend/Skills.vue
@@ -7,9 +7,9 @@ defineProps({
     <section class="bg-light-tail-100 dark:bg-dark-navy-500 py-16">
         <h2 class="text-2xl mb-8 font-semibold text-center">Acquired Skills</h2>
         <div class="container mx-auto">
-            <div class="grid grid-cols-8 md:grid-flow-col">
+            <div class="grid grid-cols-8 md:grid-cols-10">
                 <div
-                    class="flex items-center justify-center"
+                    class=""
                     v-for="skill in skills.data"
                     :key="skill.id"
                 >
