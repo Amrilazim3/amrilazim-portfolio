75931a3b153617870dee8b7347ebbb69105d3b06
Amril Azim <amrilazim@Amrils-MacBook-Pro.local>
2022-12-17 19:06:49 +0800

    add env

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,5 +1,57 @@
 @import('exolnet/laravel-envoy')
 
+@macro('deploy:no-npm')
+    assert:commit
+    deploy:starting
+        deploy:check
+        deploy:backup
+    deploy:started
+    deploy:provisioning
+        deploy:fetch
+        deploy:release
+        deploy:git
+        deploy:link
+        deploy:copy
+        deploy:composer
+    deploy:provisioned
+    deploy:building
+    deploy:built
+    deploy:publishing
+        deploy:symlink
+        deploy:publish
+        deploy:cronjobs
+    deploy:published
+    deploy:finishing
+        deploy:cleanup
+    deploy:finished
+@endmacro
+
+@task('deploy:link:files')
+    @foreach ($linkedFiles as $file)
+        @php
+            [$from, $file] = explode(":", $file);
+        @endphp
+
+        mkdir -p `dirname "{{ $sharedPath }}/{{ $from }}"`
+
+        if [ -f "{{ $releasePath }}/{{ $file }}" ]; then
+            if [ ! -f "{{ $sharedPath }}/{{ $from }}" ]; then
+                cp "{{ $releasePath }}/{{ $file }}" "{{ $sharedPath }}/{{ $from }}"
+            fi
+
+            rm -f "{{ $releasePath }}/{{ $file }}"
+        fi
+
+        if [ ! -f "{{ $sharedPath }}/{{ $from }}" ]; then
+            touch "{{ $sharedPath }}/{{ $from }}"
+        fi
+
+        mkdir -p `dirname "{{ $releasePath }}/{{ $file }}"`
+
+        ln -srfn "{{ $sharedPath }}/{{ $from }}" "{{ $releasePath }}/{{ $file }}"
+    @endforeach
+@endtask
+
 @task('deploy:publish')
     cd "{{ $releasePath }}"
 
--- config/deploy.php
+++ config/deploy.php
@@ -9,7 +9,7 @@ return [
         'production' => [
             'ssh_host'       => '128.199.131.110',
             'ssh_user'       => 'amril',
-            'deploy_path'    => '/var/www/html',
+            'deploy_path'    => '/var/www/amrilazim-portfolio',
             'repository_url' => 'git@github.com:Amrilazim3/amrilazim-portfolio.git',
             'linked_files'   => ['.env'],
             'linked_dirs'    => ['storage/app', 'storage/framework', 'storage/logs'],
